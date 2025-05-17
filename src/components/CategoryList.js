8e9842ad7cfdad215a0ded52929d7c8e2e84428a
amzorka <salbiev62@mail.ru>
2025-05-17 22:52:00 +0300

    adaptive 99% done

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import '../css/CategoryList.scss'; 
-import mixes from '../images/categories/mix.jpg';
-import general from '../images/categories/general.jpg';
-import instrument from '../images/categories/instrument.jpg';
-import paints from '../images/categories/paints.jpg';
-import finishing from '../images/categories/finishing.jpg';
-import plumbing from '../images/categories/plumbing.jpg';
-import electrogoods from '../images/categories/electrogoods.jpg';
-import isolation from '../images/categories/isolation.jpg';
-import lumber from '../images/categories/lumber.jpg';
-import metal from '../images/categories/metal.jpg';
-import { Link } from 'react-router-dom';
-
-
+import React from "react";
+import "../css/CategoryList.scss";
+import mixes from "../images/categories/mix.jpg";
+import general from "../images/categories/general.jpg";
+import instrument from "../images/categories/instrument.jpg";
+import paints from "../images/categories/paints.jpg";
+import finishing from "../images/categories/finishing.jpg";
+import plumbing from "../images/categories/plumbing.jpg";
+import electrogoods from "../images/categories/electrogoods.jpg";
+import isolation from "../images/categories/isolation.jpg";
+import lumber from "../images/categories/lumber.jpg";
+import metal from "../images/categories/metal.jpg";
+import { Link } from "react-router-dom";
 
 const categories = [
-  { name: 'Смеси строительные', imgSrc: mixes, link: '/catalog/mixes' },
-  { name: 'Общестрой', imgSrc: general, link: '/catalog/general' },
-  { name: 'Инструменты', imgSrc: instrument, link: '/catalog/instrument' },
-  { name: 'Лакокрасочные материалы', imgSrc: paints, link: '/catalog/paints' },
-  { name: 'Отделочные материалы', imgSrc: finishing, link: '/catalog/finishing' },
-  { name: 'Инженерная сантехника', imgSrc: plumbing, link: '/catalog/plumbing' },
-  { name: 'Электротовары', imgSrc: electrogoods, link: '/catalog/electrogoods' },
-  { name: 'Изоляция', imgSrc: isolation, link: '/catalog/isolation' },
-  { name: 'Пиломатериалы', imgSrc: lumber, link: '/catalog/lumber' },
-  { name: 'Металл', imgSrc: metal, link: '/catalog/metal' },
+  { name: "СМЕСИ СТРОИТЕЛЬНЫЕ", imgSrc: mixes, link: "/catalog/mixes" },
+  { name: "ОБЩЕСТРОЙ", imgSrc: general, link: "/catalog/general" },
+  { name: "ИНСТРУМЕНТЫ", imgSrc: instrument, link: "/catalog/instrument" },
+  { name: "ЛАКОКРАСОЧНЫЕ МАТЕРИАЛЫ", imgSrc: paints, link: "/catalog/paints" },
+  {
+    name: "ОТДЕЛОЧНЫЕ МАТЕРИАЛЫ",
+    imgSrc: finishing,
+    link: "/catalog/finishing",
+  },
+  {
+    name: "ИНЖЕНЕРНАЯ САНТЕХНИКА",
+    imgSrc: plumbing,
+    link: "/catalog/plumbing",
+  },
+  {
+    name: "ЭЛЕКТРОТОВАРЫ",
+    imgSrc: electrogoods,
+    link: "/catalog/electrogoods",
+  },
+  { name: "ИЗОЛЯЦИЯ", imgSrc: isolation, link: "/catalog/isolation" },
+  { name: "ПИЛОМАТЕРИАЛЫ", imgSrc: lumber, link: "/catalog/lumber" },
+  { name: "МЕТАЛЛ", imgSrc: metal, link: "/catalog/metal" },
 ];
 
 const CategoryList = () => {
@@ -32,12 +42,16 @@ const CategoryList = () => {
     <div className="category-list-container">
       <div className="categories-cards">
         {categories.map((category, index) => (
-        <Link to={category.link} key={index} className="category-card-link">
-          <div key={index} className="categories-card">
-            <img src={category.imgSrc} alt={category.name} className="categories-image" />
-            <div className="categories-name">{category.name}</div>
-          </div>
-        </Link>
+          <Link to={category.link} key={index} className="category-card-link">
+            <div key={index} className="categories-card">
+              <img
+                src={category.imgSrc}
+                alt={category.name}
+                className="categories-image"
+              />
+              <div className="categories-name">{category.name}</div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,14 +1,16 @@
 import Header from "../components/Header";
-import PartnersSection from "../components/PartnersSection";
-import InfoBlocksSection from "../components/InfoBlocksSection";
 import Footer from "../components/Footer";
 import Breadcrumbs from "../components/Breadcrumbs";
 import PageLoader from "../components/PageLoader";
 import CategoryList from "../components/CategoryList";
 import { useState, useEffect } from "react";
+import useIsMobile from "../hooks/useIsMobile";
+import MobileHeader from "../components/MobileHeader";
+import MobileFooter from "../components/MobileFooter";
 
 function Catalog() {
   const [loading, setLoading] = useState(true);
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     // Имитация загрузки 500 мс (потом можно заменить на реальную)
@@ -18,14 +20,14 @@ function Catalog() {
 
   return (
     <>
-      <Header />
+      {isMobile ? <MobileHeader /> : <Header />}
       <Breadcrumbs />
       {loading ? (
         <PageLoader />
       ) : (
         <>
           <CategoryList />
-          <Footer />
+          {isMobile ? <MobileFooter /> : <Footer />}
         </>
       )}
     </>
