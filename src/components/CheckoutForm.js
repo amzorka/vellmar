d64eb61e071b1cad9db1053ad6f5f75733ccb63c
amzorka <salbiev62@mail.ru>
2025-05-19 13:47:06 +0300

    new link test

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -19,7 +19,7 @@ const CheckoutForm = () => {
   console.log("cartItems:", cartItems);
 
   const products = cartItems.map((item) => ({
-    link: item.images?.[0]?.link || "",
+    link: `/product${item.link || ""}`,
     name: item.selectedVariant
       ? `${item.title || item.name || "Неизвестный товар"} (${
           item.selectedVariant
