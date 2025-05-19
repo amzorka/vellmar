6d2ac02c556f00b285860a4124ce594835332c22
amzorka <salbiev62@mail.ru>
2025-05-19 11:01:27 +0300

    CheckoutForm final fix

--- src/pages/SuccessPage.js
+++ src/pages/SuccessPage.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import OrderConfirmation from "../components/OrderConfirmation";
+import Header from "../components/Header";
+import Breadcrumbs from "../components/Breadcrumbs";
+import Footer from "../components/Footer";
 
 const SuccessPage = () => {
   const { state } = useLocation();
   const orderNumber = state?.orderNumber || 1000;
 
-  return <OrderConfirmation orderNumber={orderNumber} />;
+  return (
+    <>
+      <Header />
+      <Breadcrumbs />
+      <OrderConfirmation orderNumber={orderNumber} />
+      <Footer />
+    </>
+  );
 };
 
 export default SuccessPage;
