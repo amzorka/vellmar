3b7b14dd42ab795e2a25cd237ffd25b873fb38aa
amzorka <salbiev62@mail.ru>
2025-05-22 18:04:36 +0300

    confirmation mobilized

--- src/pages/SuccessPage.js
+++ src/pages/SuccessPage.js
@@ -4,17 +4,21 @@ import OrderConfirmation from "../components/OrderConfirmation";
 import Header from "../components/Header";
 import Breadcrumbs from "../components/Breadcrumbs";
 import Footer from "../components/Footer";
+import MobileHeader from "../components/MobileHeader";
+import MobileFooter from "../components/MobileFooter";
+import useIsMobile from "../hooks/useIsMobile";
 
 const SuccessPage = () => {
+  const isMobile = useIsMobile();
   const { state } = useLocation();
   const orderNumber = state?.orderNumber || 1000;
 
   return (
     <>
-      <Header />
+      {isMobile ? <MobileHeader /> : <Header />}
       <Breadcrumbs />
       <OrderConfirmation orderNumber={orderNumber} />
-      <Footer />
+      {isMobile ? <MobileFooter /> : <Footer />}
     </>
   );
 };
