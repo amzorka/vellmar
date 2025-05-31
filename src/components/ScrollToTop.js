09c300635b8410e6f825adae517c9fecca7142d2
amzorka <salbiev62@mail.ru>
2025-05-31 18:26:30 +0300

    ScrollToTop fixed

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "instant" });
-  }, [pathname]);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname, search]);
 
   return null;
 };
